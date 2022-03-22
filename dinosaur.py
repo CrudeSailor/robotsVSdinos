9b2c343dea7680a59f3bf636f2d5d3483a51c7e1
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-03-22 13:13:11 -0700

    Opponents added for game play

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+
+from weapons import Weapons
+
+class Dinosuar:
+    def __init__(self, health, weapon):
+        self.name = ['Johnny Knoxsuarus, T-rex Animal, Lazy Jerrysuarus']       
+        self.health = health
+        self.weapon = weapon
+     
---
+++ robots.py
@@ -0,0 +1,10 @@
+from weapons import Weapons
+
+
+
+
+class Robots:
+    def __init__(self, health, weapon):
+        self.name = ['Mega Boss, Short Circuit, Iron Baby']       
+        self.health = health
+        self.weapon = weapon
