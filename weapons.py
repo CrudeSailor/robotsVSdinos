6d331b0297d58acf0f0fc910663cf31f8232eddc
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-03-22 12:31:33 -0700

    Next is weapons.py for game set up

---
+++ weapons.py
@@ -0,0 +1,8 @@
+
+
+
+class Weapons:
+    def __init__(self):
+        self.name = ['comet blast, laser beam, spirit bomb, dirty knife, poison covered spork, circuit burn attack']       
+        self.damage = 10
+        
