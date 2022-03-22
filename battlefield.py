c6f6f128e0c523ed03a7bef69c795204b39ee975
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-03-22 13:15:52 -0700

    last additionfor game play: Battlefield

---
+++ battlefield.py
@@ -0,0 +1,11 @@
+from dinosuar import Dinosaur
+
+
+
+
+class Battlefield:    
+    def __init__(self, location):
+        
+
+if health is == 0:
+    print('The Dinosuars have gone extinct, again, Thanks Terminators!!!!')        
