e39607f45669b436bb96493c30e3fb3d4d026218
Rafael Diaz <crudesailorphotographysd@gmail.com>
2022-03-22 15:12:30 -0700

    battlefield update, saved progress for all classes and needs small updates

--- battlefield.py
+++ battlefield.py
@@ -1,11 +1,29 @@
-from dinosuar import Dinosaur
+from fleet import Fleet
+
+from herd import Herd
+
 
 
 
 
 class Battlefield:    
-    def __init__(self, location):
-        
+    def __init__(self): None
 
-if health is == 0:
-    print('The Dinosuars have gone extinct, again, Thanks Terminators!!!!')        
+    def run_game(self): None
+
+    def display_welcome(self): None
+
+    def battle(self): None
+
+    def dino_turn(self, dinosaur): None  
+          
+        
+    def robo_turn(self, robot): None
+
+    def show_dino_opponent_options(self): None
+
+    def show_robo_opponent_options(self): None
+
+    def display_winners(self): None
+
+      
